38f655bbf0424fc35969e760e2088f676afa0eaf
Joseph <95051755+Youxise@users.noreply.github.com>
2025-02-02 14:48:01 +0100

    using connection instead of engine

--- src/db_setup.py
+++ src/db_setup.py
@@ -13,6 +13,7 @@ DB_NAME = "rec_db"
 # Create SQLAlchemy engine
 DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 engine = create_engine(DATABASE_URL, pool_recycle=3600)
+connection = engine.raw_connection()
 
 print("Creating tables and inserting data...")
 
@@ -29,9 +30,7 @@ for table, filename in csv_files.items():
     file_path = os.path.join(data_path, filename)
     if os.path.exists(file_path):  # Ensure file exists
         df = pd.read_csv(file_path)
-        print(df.columns)
-        print(df)
-        df.to_sql(table, engine, if_exists="replace", index=False)
+        df.to_sql(table, connection, if_exists="replace", index=False)
         print(f"Inserted data into {table}")
     else:
         print(f"File {file_path} not found!")
